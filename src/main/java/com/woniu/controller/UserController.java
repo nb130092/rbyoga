5f249d1b9e7d91e95448561d7478a9aaceb1d6bd
gitee-liuxinyu <6519106+lxy940604@user.noreply.gitee.com>
2020-03-26 22:12:57 +0800

    lxy/dev:完成用户表的crud

--- src/main/java/com/woniu/controller/UserController.java
+++ src/main/java/com/woniu/controller/UserController.java
@@ -4,7 +4,6 @@ import com.woniu.pojo.ResultVO;
 import com.woniu.pojo.User;
 import com.woniu.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
