ebd4217acbbc9783e63086caade0d3a812a781e5
gitee-liuxinyu <6519106+lxy940604@user.noreply.gitee.com>
2020-03-28 12:20:41 +0800

    lxy/dev:删除无用引入文件

--- src/main/java/com/woniu/controller/ProductController.java
+++ src/main/java/com/woniu/controller/ProductController.java
@@ -4,7 +4,6 @@ import com.woniu.pojo.Product;
 import com.woniu.pojo.ResultVO;
 import com.woniu.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
