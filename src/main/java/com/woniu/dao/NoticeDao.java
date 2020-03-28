90a1aa0c12576b3874b23035ff623be128ff15ab
zhang1667763448 <1667763448@qq.com>
2020-03-28 15:31:25 +0800

    修改mapper注解

--- src/main/java/com/woniu/dao/NoticeDao.java
+++ src/main/java/com/woniu/dao/NoticeDao.java
@@ -1,10 +1,12 @@
 package com.woniu.dao;
 
 import com.woniu.pojo.Notice;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
+@Mapper
 public interface NoticeDao {
     List<Notice> findAll();
     Notice findOne(Integer u_id);
